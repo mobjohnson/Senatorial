e6024f7524c292c07e137147608a2539b46197ec
mobjohnson <mobjohnson@gmail.com>
2014-10-27 15:03:07 -0400

    Add search_items_controller

---
+++ app/controllers/search_items_controller.rb
@@ -0,0 +1,35 @@
+class SearchItemsController < ApplicationController
+
+  before_action :set_search_item, only: [:show, :update, :destroy]
+
+  def index
+    render json: SearchItem.all, statys: 200
+  end
+
+  def show
+    render json: @search_item.to_json, status: 200
+  end
+
+  def create
+    @search_item = SearchItem.new(search_item_params)
+    render json: @search_item.to_json, status: 200 if @search_item.save
+  end
+
+  def update
+    render json: @search_item.to_json, status: 200 if @search_item.update(search_item_params)
+  end
+
+  def destroy
+    render json: @search_item.to_json if @search_item.destroy
+  end
+
+  private
+
+  def set_search_item
+    @search_item = SearchItem.find(params[:id])
+  end
+
+  def search_item_params
+    params.require(:search_item).permit(:state1, :state2, :phrase, :results1, :results2)
+  end
+end
