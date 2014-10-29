e7318cd687217bd93e459641205a2a05c043c08d
mobjohnson <mobjohnson@gmail.com>
2014-10-29 13:47:48 -0400

    Add senatorCollection.js

---
+++ app/assets/javascripts/collections/senatorCollection.js
@@ -0,0 +1,7 @@
+App.SenatorCollection = Backbone.Collection.extend({
+  model: App.SenatorModel,
+  url: '/senators',
+  initialize: function(){
+    console.log('New Senator Collection');
+  }
+});
