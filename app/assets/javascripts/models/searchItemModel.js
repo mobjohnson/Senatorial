3067cc6bb0492d0884800bef1b28805888c086b5
mobjohnson <mobjohnson@gmail.com>
2014-10-30 17:08:51 -0400

    Modify searchItemModel.js for deeplink

--- app/assets/javascripts/models/searchItemModel.js
+++ app/assets/javascripts/models/searchItemModel.js
@@ -1,6 +1,9 @@
 App.SearchItemModel = Backbone.Model.extend({
   initialize: function(params){
     console.log('search item data', params);
+    this.state1 = params.state1;
+    this.state2 = params.state2;
+    this.phrase = params.phrase;
     this.state1Senator = new App.SenatorModel({state: params.state1});// adding attributes to this model
     this.state2Senator = new App.SenatorModel({state: params.state2});
   },
