8decf8911a0af1477812aaed6e097d934874a99d
mobjohnson <mobjohnson@gmail.com>
2014-10-30 11:23:29 -0400

    Modify congressRecordCollection.js for successful return

--- app/assets/javascripts/collections/congressRecordCollection.js
+++ app/assets/javascripts/collections/congressRecordCollection.js
@@ -4,22 +4,22 @@ App.CongressRecordCollection = Backbone.Collection.extend({
   url: '/congressRecords',
   parse: function(data){
     return data.objects;
-  }
+  },
 
   initialize: function(){
     console.log('New Congress Record Collection');
   }
 
-  var congressRecords = new App.CongressRecordCollection();
+  // var congressRecords = new App.CongressRecordCollection();
 
-  congressRecords.fetch().then(function(){
-    // If senator_rank = senior
-    // person.name
-    // person.bioguideid
-    // 
-    console.log('inside fetch');
-    console.log(this);
+  // congressRecords.fetch().then(function(){
+  //   // If senator_rank = senior
+  //   // person.name
+  //   // person.bioguideid
+  //   // 
+  //   console.log('inside congress record fetch');
+  //   console.log(this);
 
-  });
+  // });
 
 });
