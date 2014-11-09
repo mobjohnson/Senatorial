18ab6af68ac3d7815fde0e05f60156a5073d6254
mobjohnson <mobjohnson@gmail.com>
2014-11-09 06:14:44 -0500

    Comment senatorModel.js

--- app/assets/javascripts/models/searchItemModel.js
+++ app/assets/javascripts/models/searchItemModel.js
@@ -1,11 +1,14 @@
 App.SearchItemModel = Backbone.Model.extend({
   initialize: function(params){
     console.log('search item data', params);
+    // add state and phrase tp params from router call
     this.state1 = params.state1;
     this.state2 = params.state2;
     this.phrase = params.phrase;
-    this.state1Senator = new App.SenatorModel({state: params.state1});// adding attributes to this model
-    this.state2Senator = new App.SenatorModel({state: params.state2});
+    // create new SenatorModel and adding attributes to this model for state1Senator
+    this.state1Senator = new App.SenatorModel({state: params.state1});
+    // creating new SenatorModel and adding attributes to this model for state1Senato2
+    this.state2Senator = new App.SenatorModel({state: params.state2}); 
   },
 
 
