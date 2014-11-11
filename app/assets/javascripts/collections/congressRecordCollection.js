83a3930672b25df2314700c1a51c60efc8a82657
mobjohnson <mobjohnson@gmail.com>
2014-11-11 18:05:27 -0500

    Edit congressRecord and senator Collections.js for initializing collections

--- app/assets/javascripts/collections/congressRecordCollection.js
+++ app/assets/javascripts/collections/congressRecordCollection.js
@@ -2,9 +2,9 @@ App.CongressRecordCollection = Backbone.Collection.extend({
 
   model: App.CongressRecordModel,
   url: '/congress_records',
-  parse: function(data){
-    return data.objects;
-  },
+  // parse: function(data){
+  //   return data.objects;
+  // },
 
   initialize: function(){
     console.log('New Congress Record Collection');
