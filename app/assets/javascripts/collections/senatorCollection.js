983a2dec423ed5f4156bbf2d715a711decaa03c1
mobjohnson <mobjohnson@gmail.com>
2014-11-11 14:23:49 -0500

    Modify senatorCollection for intermediate waypoint

--- app/assets/javascripts/collections/senatorCollection.js
+++ app/assets/javascripts/collections/senatorCollection.js
@@ -2,9 +2,9 @@ App.SenatorCollection = Backbone.Collection.extend({
 
   model: App.SenatorModel,
   url: '/senators',
-  parse: function(data){
-    return data.objects;
-  },
+  // parse: function(data){
+  //   return data.objects;
+  // },
 
   initialize: function(){
     console.log('New Senator Collection');
