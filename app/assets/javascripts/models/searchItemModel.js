a67a02f4203877873cd1fe301bf7c4055e652286
mobjohnson <mobjohnson@gmail.com>
2014-11-11 22:21:47 -0500

    Modify congressRecors Model and ListView for visible chart

--- app/assets/javascripts/models/searchItemModel.js
+++ app/assets/javascripts/models/searchItemModel.js
@@ -2,13 +2,20 @@ App.SearchItemModel = Backbone.Model.extend({
   initialize: function(params){
     console.log('searchItemModel (#initialize) params:', params);
     // add state and phrase to params from router call, used to instantiate senator model
-    this.state1 = params.state1;
-    this.state2 = params.state2;
-    this.phrase = params.phrase;
+    // this.state1 = params.state1;
+    // this.state2 = params.state2;
+    // this.phrase = params.phrase;
     // create new senatorModel and adding attributes to this model for state1Senator
-    this.state1Senator = new App.SenatorModel({phrase: params.phrase, state: params.state1});
+    if (params.state1){
+      this.state1Senator = new App.SenatorModel({phrase: params.phrase, state: params.state1});
+    }
     // creating new senatorModel and adding attributes to this model for state1Senator
-    this.state2Senator = new App.SenatorModel({phrase: params.phrase, state: params.state2}); 
+    if (params.state2){
+      this.state2Senator = new App.SenatorModel({phrase: params.phrase, state: params.state2});       
+    }
+
+
+
   },
 
 
