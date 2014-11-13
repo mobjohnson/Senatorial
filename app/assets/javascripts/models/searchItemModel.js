803ff1be777c0b33b6a7a520aab9fdcfbb7c0537
mobjohnson <mobjohnson@gmail.com>
2014-11-13 04:22:46 -0500

    Modify files for 2nd intermediate work on charts

--- app/assets/javascripts/models/searchItemModel.js
+++ app/assets/javascripts/models/searchItemModel.js
@@ -2,9 +2,9 @@ App.SearchItemModel = Backbone.Model.extend({
   initialize: function(params){
     console.log('searchItemModel (#initialize) params:', params);
     // add state and phrase to params from router call, used to instantiate senator model
-    // this.state1 = params.state1;
-    // this.state2 = params.state2;
-    // this.phrase = params.phrase;
+    this.state1 = params.state1;
+    this.state2 = params.state2;
+    this.phrase = params.phrase;
     // create new senatorModel and adding attributes to this model for state1Senator
     if (params.state1){
       this.state1Senator = new App.SenatorModel({phrase: params.phrase, state: params.state1});
