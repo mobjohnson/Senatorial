8f41db30276ff7768cef1a26618518facddfd161
mobjohnson <mobjohnson@gmail.com>
2014-11-18 12:22:04 -0500

    Modify senatorModel for complete congressrecords collection

--- app/assets/javascripts/models/senatorModel.js
+++ app/assets/javascripts/models/senatorModel.js
@@ -54,7 +54,7 @@ App.SenatorModel = Backbone.Model.extend({
               } else {
    
               }
-              console.log('senatorModel (data.changed.objects[0].person.name) this.sr_senator: ',this.sr_senator);
+              console.log('**** senatorModel (data.changed.objects[0].person.name) this.sr_senator: ',this.sr_senator);
               // sets jr_senator based upon data.changed
               if (data.changed.objects[1]) {
                 this.jr_senator = data.changed.objects[1].person.name;
@@ -64,24 +64,24 @@ App.SenatorModel = Backbone.Model.extend({
 
 
               }
-              console.log('senatorModel (data.changed.objects[1].person.name) this.jr_senator: ', this.jr_senator);
+              console.log('**** senatorModel (data.changed.objects[1].person.name) this.jr_senator: ', this.jr_senator);
 
-              console.log('senatorModel (data.changed.objects[0].person.bioguideid) this.sr_member_ref: ', this.sr_member_ref);       
+              console.log('**** senatorModel (data.changed.objects[0].person.bioguideid) this.sr_member_ref: ', this.sr_member_ref);       
 
-              console.log('senatorModel (data.changed.objects[1].person.bioguideid) this.jr_member_ref: ', this.jr_member_ref);  
+              console.log('**** senatorModel (data.changed.objects[1].person.bioguideid) this.jr_member_ref: ', this.jr_member_ref);  
               // creating new congressRecordModel and adding sttributes to this model for sr_senator
               this.Record1 = new App.CongressRecordModel({phrase: params.phrase, senator_name: this.sr_senator, bioguideid: this.sr_member_ref});
               self.parentObject.congressrecords.add(this.Record1);
               // creating new congressRecordModel and adding sttributes to this model for jr_senator
-              this.Record1 = new App.CongressRecordModel({phrase: params.phrase, senator_name: this.jr_senator, bioguideid: this.jr_member_ref});
+              this.Record2 = new App.CongressRecordModel({phrase: params.phrase, senator_name: this.jr_senator, bioguideid: this.jr_member_ref});
               self.parentObject.congressrecords.add(this.Record2);
 
               // Add sr_senator to Linechart
               App.LineChart.push([this.sr_senator]);
-              console.log('<<<<senatorModel LineChart:', App.LineChart)
+              console.log('senatorModel LineChart:', App.LineChart)
               // Add jr_senator to Linechart
               App.LineChart.push([this.jr_senator]);
-              console.log('>>>>senatorModel LineChart:', App.LineChart);
+              console.log('senatorModel LineChart:', App.LineChart);
             },
 
           });
