fb1d4360de546bac08cd8c30a41bf6d3899fcfbe
mobjohnson <mobjohnson@gmail.com>
2014-11-20 15:39:25 -0500

    Modify conrgessRecordModel to delete defaults

--- app/assets/javascripts/models/congressRecordModel.js
+++ app/assets/javascripts/models/congressRecordModel.js
@@ -4,14 +4,6 @@ App.CongressRecordModel = Backbone.Model.extend({
     return "http://capitolwords.org/api/1/dates.json?phrase=" + this.phrase + "&bioguide_id=" + this.bioguideid + "&start_date= 2014-03-31&end_date=2014-11-01&granularity=month&apikey=3fdb489020354ae7b4e1a1bf75b4a279"
   },
 
-  // Defaults for name and bioguideid for Capital Words API, and placeholder for array return
-  // defaults:{
-  //   phrase: '',
-  //   senator_name: '',
-  //   bioguideid: '',
-  //   recordCounts: [] 
-  // },
-
   initialize: function(params){
     if (params.phrase === undefined){
     } else {
@@ -22,6 +14,7 @@ App.CongressRecordModel = Backbone.Model.extend({
       console.log('congressRecordModel (#initialize) this.senator_name: ', this.senator_name);
       console.log('congressRecordModel (#initialize) this.bioguideid: ', this.bioguideid);
       console.log('congressRecordModel (#initialize) this:', this);
+
       // do fetch
       if (this.phrase){
         this.fetch({
@@ -31,32 +24,9 @@ App.CongressRecordModel = Backbone.Model.extend({
             console.log('congressRecordModel (#initialize/this.fetch/#success this:', this);
 
             // set recordCounts to array of Congress Records matching criteria.
-            // recordCounts includes both nubers of records and month.
+            // recordCounts includes both numbers of records and  the months.
             this.recordCounts = data.attributes.results;
             console.log('congressRecordModel (initialize/this.fetch/#success data.attributes.results: ', this.recordCounts);
-
-            // // store array of numbers from recordCounts into numArray
-            // this.numArray = [];  
-           
-            // // push recordCount numbers into numArray
-            // for (var i = 0; i < this.recordCounts.length; i++) {
-            //   this.numArray.push(this.recordCounts[i].count);
-            // };
-            // // console.log('congressRecordModel numArray:', this.numArray);  
-            // // need to move to outer scope for this.senator_name
-
-            // // Add recordCounts to LineChart
-            // // console.log('congressRecordModel App.LineChart', App.LineChart);
-
-            // // Add numArray to LineChart
-            // for (var i = 0; i < App.LineChart.length; i++) {
-            //   if (App.LineChart[i][0] === data.senator_name) {
-            //     App.LineChart[i] = App.LineChart[i].concat(this.numArray);
-            //   }; 
-            // };
-            
-            // // console.log('congressRecordModel LineChart:', App.LineChart);
-
           }
         });
       }
