fc11c7b6cf05aac9487fcf5b370575f732b181ca
mobjohnson <mobjohnson@gmail.com>
2015-01-14 11:38:05 -0500

    Remove senator model and controller from rails

--- app/models/search_item.rb
+++ app/models/search_item.rb
@@ -1,4 +1,3 @@
 class SearchItem < ActiveRecord::Base
   belongs_to :user
-  has_and_belongs_to_many :senators
 end
--- app/models/senator.rb
+++
@@ -1,3 +0,0 @@
-class Senator < ActiveRecord::Base
-  has_and_belongs_to_many :search_items
-end
